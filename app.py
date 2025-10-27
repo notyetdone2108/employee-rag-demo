b86fe75af156814967b31c607f2161cf65c30ec6
notyetdone2108 <84870249+notyetdone2108@users.noreply.github.com>
2025-10-27 12:31:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,19 +4,6 @@ import requests
 st.title ("streamlit connectivity test")
 st.write("connect success")  
 
-databricks_host = "https://dbc-c57904c6-817b.cloud.databricks.com"
-databricks_token = "dapi7db165329b88817345a287fdaa60b3b0"
-
-headers = headers = {"Authorization": f"Bearer {databricks_token}"}
-st.write("testing host", databricks_host)
-
-try:
-  r = requests.get(f"{databricks_host}/api/2.0/clusters/list", timeout=8)
-  if r.status_code == 401:
-    st.waarning("datbricks reachable but unauthorised")
-  elif r.status_code == 200:
-    st.success("reachable")
-  else:
-    st.warning("responded with status code {r.status_code}")
-except Exception as e:
-  st.error(f"couldnt reach databricks")
+name = st.text_input("enter your name : ")
+if name:
+    st.success ("your app is fine")
