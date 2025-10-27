aec4969eb951f2721bbaa6306bde02dcac4b3337
notyetdone2108 <84870249+notyetdone2108@users.noreply.github.com>
2025-10-27 09:50:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,13 @@ import requests
 
 st.title ("streamlit connectivity test")
 st.write("connect success")  
+
+databricks_host = "https://dbc-c57904c6-817b.cloud.databricks.com"
+try:
+  r = requests.get(f"{databricks_host}/api/2.0/clusters/list", timeout=8)
+  if r.status_code == 401:
+    st.waarning("datbricks reachable but unauthorised")
+  elif r.status_code == 200:
+    st.success("reachable")
+except Exception as e:
+  st.error(f"couldnt reach databricks")
