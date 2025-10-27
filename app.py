cb62e79a2f1685857ee0ce1c1084fb67418d73b0
notyetdone2108 <84870249+notyetdone2108@users.noreply.github.com>
2025-10-27 10:03:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ st.title ("streamlit connectivity test")
 st.write("connect success")  
 
 databricks_host = "https://dbc-c57904c6-817b.cloud.databricks.com"
+databricks_token = "dapi7db165329b88817345a287fdaa60b3b0"
+
+headers = headers = {"Authorization": f"Bearer {databricks_token}"}
+st.write("testing host", databricks_host)
+
 try:
   r = requests.get(f"{databricks_host}/api/2.0/clusters/list", timeout=8)
   if r.status_code == 401:
